e6900f364c697f16faf8893070c1c8b0fd7f7d02
yashviradia <63194797+yashviradia@users.noreply.github.com>
2021-10-16 10:01:43 +0200

    add files snake game

---
+++ scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+
+SCORE = 0
+ALIGNMENT = "center"
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        self.high_score = 0
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=("Courier", 24, "normal"))
+
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER.", align=ALIGNMENT, font=("Courier", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
